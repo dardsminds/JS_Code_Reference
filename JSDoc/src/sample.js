99bbf27e52177d4c6fbaf83d0811a1616f006ab3
dardsminds <106987969+dardsminds@users.noreply.github.com>
2025-03-24 22:15:57 -0500

    Create sample.js

---
+++ JSDoc/src/sample.js
@@ -0,0 +1,11 @@
+/**
+ * Retrieves a user by email.
+ * @async
+ * @method
+ * @param {String} email - User email
+ * @returns {User} User object
+ * @throws {NotFoundError} When the user is not found.
+ */
+const getByEmail = async (email) => {
+    console.log("test");
+}
